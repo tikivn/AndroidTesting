9e212c373ea5f73c1f68fdc527b5bcebe049dc0d
Tale <giangnguyen.tale@gmail.com>
2018-06-07 14:32:31 +0700

    init project

---
+++ app/src/main/java/vn/tiki/android/androidtesting/MainActivity.kt
@@ -0,0 +1,13 @@
+package vn.tiki.android.androidtesting
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.main_activity)
+  }
+
+}
