b3cf85060c57f291a818a4d6db4680c827826886
Tale <giangnguyen.tale@gmail.com>
2018-06-09 05:23:35 +0700

    add CustomTestRunner to use TestApp for testing

---
+++ app/src/androidTest/java/vn/tiki/android/androidtesting/CustomTestRunner.kt
@@ -0,0 +1,14 @@
+package vn.tiki.android.androidtesting
+
+import android.app.Application
+import android.content.Context
+import android.support.test.runner.AndroidJUnitRunner
+
+/**
+ * Custom runner to disable dependency injection.
+ */
+class CustomTestRunner : AndroidJUnitRunner() {
+  override fun newApplication(cl: ClassLoader, className: String, context: Context): Application {
+    return super.newApplication(cl, TestApp::class.java.name, context)
+  }
+}
