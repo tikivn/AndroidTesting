41b3fb5f34330a6ec4e5a78f0002cde1fe791e1e
Tale <giangnguyen.tale@gmail.com>
2018-06-08 15:15:25 +0700

    add LocalStorageImpl to implement LocalStorage

---
+++ app/src/main/java/vn/tiki/android/androidtesting/data/local/LocalStorageImpl.kt
@@ -0,0 +1,32 @@
+package vn.tiki.android.androidtesting.data.local
+
+import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.MutableLiveData
+import android.content.SharedPreferences
+import androidx.core.content.edit
+import vn.tiki.android.androidtesting.data.AppExecutors
+
+class LocalStorageImpl(
+    private val appExecutors: AppExecutors,
+    private val sharedPreferences: SharedPreferences
+) : LocalStorage {
+
+  companion object {
+    const val KEY_ACCESS_TOKEN = "access_token"
+  }
+
+  override fun setAccessToken(accessToken: String) {
+    sharedPreferences.edit {
+      putString(KEY_ACCESS_TOKEN, accessToken)
+    }
+  }
+
+  override fun getAccessToken(): LiveData<String> {
+    val result = MutableLiveData<String>()
+    appExecutors.diskIO().execute {
+      val accessToken = sharedPreferences.getString(KEY_ACCESS_TOKEN, null)
+      result.postValue(accessToken)
+    }
+    return result
+  }
+}
